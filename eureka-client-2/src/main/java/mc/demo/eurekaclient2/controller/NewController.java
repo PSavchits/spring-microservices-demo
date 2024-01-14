c90c5fde7b524f0f9db6c560b7e1201fd944e0f4
PSavchits <paulloord@gmail.com>
2024-01-14 19:04:20 +0300

    Initial commit

---
+++ eureka-client-2/src/main/java/mc/demo/eurekaclient2/controller/NewController.java
@@ -0,0 +1,15 @@
+package mc.demo.eurekaclient2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class NewController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
